ca79f0cca215a22a96bb277aa9441b1f32cedeb1
Ayman Alharbi <Ayman.f.alharbi@gmail.com>
2025-05-07 22:29:33 +0300

    added StableOwner classes

---
+++ src/main/java/com/example/saheel/Repository/StableOwnerRepository.java
@@ -0,0 +1,10 @@
+package com.example.saheel.Repository;
+
+import com.example.saheel.Model.StableOwner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StableOwnerRepository extends JpaRepository<StableOwner, Integer> {
+    StableOwner findStableOwnerById(Integer stableOwnerId);
+}
