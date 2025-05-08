fe3ea12360ecf529cacf5c7bc0e4ef9abe595a89
Ayman Alharbi <Ayman.f.alharbi@gmail.com>
2025-05-08 10:35:32 +0300

    Completed CourseReview

--- src/main/java/com/example/saheel/Service/CourseReviewService.java
+++ src/main/java/com/example/saheel/Service/CourseReviewService.java
@@ -1,10 +1,8 @@
 package com.example.saheel.Service;
 
-import com.example.saheel.Model.Course;
-import com.example.saheel.Model.CourseReview;
-import com.example.saheel.Model.User;
-import com.example.saheel.Repository.CourseRepository;
-import com.example.saheel.Repository.CourseReviewRepository;
+import com.example.saheel.Api.ApiException;
+import com.example.saheel.Model.*;
+import com.example.saheel.Repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,28 +13,70 @@ import java.util.List;
 public class CourseReviewService {
     private final CourseReviewRepository courseReviewRepository;
     private final HelperService helperService;
+    private final CourseEnrollmentRepository courseEnrollmentRepository;
+    private final CustomerRepository customerRepository;
+    private final StableOwnerRepository stableOwnerRepository;
 
-    public List<CourseReview> getAllCourseReviewsByAdmin(String adminUsername, Integer courseId) {
-        // Check if the admin exists in the database.
-        helperService.getAdminOrThrow(adminUsername);
+    public List<CourseReview> getAllCourseReviewsByStableOwner(Integer stableOwnerId, Integer courseId) {
+        // Get the owner object and check if it's in the database.
+        StableOwner stableOwner = stableOwnerRepository.findStableOwnerById(stableOwnerId);
+        if (stableOwner == null) throw new ApiException("Owner not found.");
 
         // Get the course and check if it's in the database.
         Course course = helperService.getCourseOrThrow(courseId);
 
-        // Return the courses
+        // Check if the course belongs to the owner
+        if (!stableOwner.getStables().contains(course.getStable()))
+            throw new ApiException("The course does not belong to the owner.");
+
+        // Return the reviews
         return courseReviewRepository.findCourseReviewsByCourse(course);
     }
 
-    public void ReviewCourseByCustomer(Integer customerId, Integer courseId){
+    public void ReviewCourseByCustomer(Integer customerId, Integer courseId, CourseReview courseReview) {
         // Get the customer and check if it's in the database.
+        Customer customer = customerRepository.findCustomerById(customerId);
+        if (customer == null) throw new ApiException("Customer not found.");
 
         // Get the course and check if it's in the database.
+        Course course = helperService.getCourseOrThrow(courseId);
 
-        // Check if the customer enrolled in the course
+        // Check if the customer enrolled in the course.
+        helperService.checkIfCustomerEnrolled(course, customer);
 
-        // Create the review
+        // Set the customer
+        courseReview.setCustomer(customer);
+
+        // Set the course
+        courseReview.setCourse(course);
 
         // Save the review.
+        courseReviewRepository.save(courseReview);
+    }
+
+    public void updateReview(Integer customerId, Integer courseReviewId, CourseReview courseReview) {
+        // Get the customer and check if it's in the database.
+        Customer customer = customerRepository.findCustomerById(customerId);
+        if (customer == null) throw new ApiException("Customer not found.");
+
+        // Get the courseReview and check if it's in the database.
+        CourseReview oldCourseReview = courseReviewRepository.findCourseReviewById(courseReviewId);
+
+        // Get the course and check if it's in the database.
+        Course course = helperService.getCourseOrThrow(courseReview.getCourse().getId());
+
+        // Get the CourseEnrollment Object using the course and the customer.
+        CourseEnrollment courseEnrollment = courseEnrollmentRepository.findCourseEnrollmentByCourseAndCustomer(course, customer);
+
+        // Check if the customer enrolled in the course * if the courseEnrollment is null than the customer has not attended this course.
+        if (courseEnrollment == null) throw new ApiException("Customer has not attended this course.");
+
+        // Update
+        oldCourseReview.setRating(courseReview.getRating());
+        oldCourseReview.setDescription(courseReview.getDescription());
+
+        // Save the updated review.
+        courseReviewRepository.save(oldCourseReview);
     }
 
 }
