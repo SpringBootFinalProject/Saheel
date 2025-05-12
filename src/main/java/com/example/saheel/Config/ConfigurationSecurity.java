830b873bd2c208b061a5a0f0ab108780ae9adecd
AbrarSaud <abrar.sauod0@gmail.com>
2025-05-12 18:06:29 +0300

    add AWS

--- src/main/java/com/example/saheel/Config/ConfigurationSecurity.java
+++ src/main/java/com/example/saheel/Config/ConfigurationSecurity.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -144,6 +145,11 @@ public class ConfigurationSecurity {
                 .httpBasic();
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
